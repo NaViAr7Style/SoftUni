fa42fa55fcb86c721646892535a9dbc05b475bd3
NaViAr7Style <ar7styleblizz@gmail.com>
2023-06-23 17:35:45 +0300

    Upload project IntroToHibernateStandard

--- java-databases-basics/Spring Data/HibernateIntro/src/main/java/Main.java
+++ java-databases-basics/Spring Data/HibernateIntro/src/main/java/Main.java
@@ -1,7 +1,37 @@
+import entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Configuration cfg = new Configuration();
+        cfg.configure();
+
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Student student = new Student("Gosho", 15);
+        session.save(student);
+
+        Student student1 = session.get(Student.class, 1);
+        System.out.println(student1);
+
+        List<Student> allStudents = session
+                .createQuery("FROM Student WHERE name = 'Gosho'", Student.class)
+                .stream()
+                .toList();
+
+        allStudents.forEach(System.out::println);
+
+        session.getTransaction().commit();
+        session.close();
 
     }
 
