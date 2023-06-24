076d9664355443e83a28e340340bb180342fc023
NaViAr7Style <ar7styleblizz@gmail.com>
2023-06-24 14:20:15 +0300

    Upload project HibernateIntroExercise

--- java-databases-basics/Spring Data/HibernateJPAIntro/src/main/java/Main.java
+++ java-databases-basics/Spring Data/HibernateJPAIntro/src/main/java/Main.java
@@ -1,8 +1,28 @@
+import entities.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hi");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate-jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
 
+        Student student = new Student("Kani");
+        em.persist(student);
+
+        em.getTransaction().commit();
+
+        List<Student> studentList = new ArrayList<>();
+
+        studentList.add(student);
+
+        studentList.forEach(System.out::println);
     }
 }
