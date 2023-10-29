fcaf0a106b74f8f6b00a07c045388471fc6c5124
NaViAr7Style <ar7styleblizz@gmail.com>
2023-10-29 21:34:50 +0200

    Adding more custom validators.

--- spring-fundamentals/mobilele/src/main/java/org/softuni/mobilele/models/dtos/UserRegistrationDTO.java
+++ spring-fundamentals/mobilele/src/main/java/org/softuni/mobilele/models/dtos/UserRegistrationDTO.java
@@ -1,9 +1,30 @@
 package org.softuni.mobilele.models.dtos;
 
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String email,
-                                  String password,
-                                  String confirmPassword) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.softuni.mobilele.models.validation.FieldMatch;
+import org.softuni.mobilele.models.validation.UniqueUserEmail;
 
+@FieldMatch(
+        first = "password",
+        second = "confirmPassword",
+        message = "Passwords should match."
+)
+public record UserRegistrationDTO(
+
+        @NotEmpty
+        String firstName,
+
+        @NotEmpty
+        String lastName,
+
+        @NotNull
+        @Email
+        @UniqueUserEmail
+        String email,
+
+        String password,
+
+        String confirmPassword) {
 }
